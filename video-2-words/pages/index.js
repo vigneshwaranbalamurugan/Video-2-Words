0b49669fcb29b3f1147d521f9141b59e07852b89
Vigneshwaran B <vigneshwaranb.22cse@kongu.edu>
2024-12-27 19:33:13 +0530

    Altered index

--- video-2-words/pages/index.js
+++ video-2-words/pages/index.js
@@ -1,14 +1,18 @@
 
 import Head from "next/head";
-import Homepage from './home';
-import MainWorkflow from './mainworkflow';
-import Footer from './footer';
+import Homepage from "../components/home";
+import MainWorkflow from "../components/mainworkflow";
+import Footer from "../components/footer";
+
 export default function Home() {
   return (
   <>
-  <Head>
+     <Head>
         <title>Video2Words</title>
         <meta name="description" content="Welcome to my awesome website built with Next.js" />
+        <link rel="icon" type="image/jpeg" sizes="32x32" href="/video2words.jpeg" />
+        <link rel="icon" type="image/jpeg" sizes="16x16" href="/video2words.jpeg" />
+        {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" /> */}
       </Head>
   <Homepage/>
   <MainWorkflow/>
