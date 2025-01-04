df320c81e8c987b3bf01e5721b460707a71430eb
Vigneshwaran B <vigneshwaranb.22cse@kongu.edu>
2025-01-04 10:14:01 +0530

    apirequests

---
+++ video-2-words/src/apiRequests/handleFileUploadRequests.js
@@ -0,0 +1,69 @@
+import { API_Video_Upload } from "@apirequest/apiUrl";
+
+const handleFileUploadRequest = async (
+  e,
+  setUploadProgress,
+  setUploadedVideo,
+  setS3url,
+  sets3Key,
+  showToast
+) => {
+
+    const file = e.target.files[0];
+    setUploadProgress(0); 
+    let interval;
+    if (file) {
+  
+      const formData = new FormData();
+      formData.append("file", file);
+      const videoURL = URL.createObjectURL(file);
+
+      try {
+        interval = setInterval(() => {
+          setUploadProgress((prev) => {
+            if (prev >= 90) {
+              clearInterval(interval); 
+              return prev;
+            }
+            return prev + 5; 
+          });
+        }, 1000); 
+  
+        const response = await fetch(`${API_Video_Upload}`, {
+          method: "POST",
+          body: formData,
+        });
+
+        clearInterval(interval); 
+        setUploadProgress(90);
+        
+        if (response.ok) {
+          const data = await response.json();
+          setUploadedVideo(videoURL);
+
+          setTimeout(() => {
+          }, 2000);
+      
+          setUploadProgress(100); 
+          showToast("Video Uploaded Sucessfully","success");
+          setS3url(data.url);
+          sets3Key(data.s3Key);
+
+        } else {
+          const error = await response.json();
+          setUploadProgress(0);
+          clearInterval(interval); 
+
+          showToast("Error Uploading Video,Please try again..!","error");
+        }
+      } catch (error) {
+        setUploadProgress(0); 
+        clearInterval(interval); 
+
+        showToast("Please try again later..!","error");
+      }
+    }
+  };
+
+
+export default handleFileUploadRequest;
---
+++ video-2-words/src/apiRequests/handleGetTranscriptRequests.js
@@ -0,0 +1,48 @@
+import { API_Get_Transcript } from "@apirequest/apiUrl";
+
+const handleGetTranscriptRequest = async(
+  uploadProgress,
+  s3Key,
+  selectedLanguage,
+  setOriginalTranscript,
+  setTranslatedTranscript,
+  setTranscriptionReady,
+  setdetectedLanguage,
+  showToast
+) =>{
+    if(uploadProgress!=100)
+    {
+      showToast("Upload Video to Continue!", "warn");
+      return;
+    }
+    try {
+        const response = await fetch(`${API_Get_Transcript}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            s3Key:s3Key,
+            targetLanguage:selectedLanguage,
+          }),
+        });
+  
+        if (response.ok) {
+          const data = await response.json(); 
+          showToast(data.message,"success");
+          setOriginalTranscript(data.originalTranscript);
+          setTranslatedTranscript(data.translatedTranscript);
+          setdetectedLanguage(data.detectedLanguage);
+          setTranscriptionReady(true);
+
+        } else {
+          const errorData = await response.json(); 
+          showToast(errorData.message,"error");
+        }
+      } catch (err) {
+        showToast("Failed to connect to the server. Please try again.!", "error");
+      } 
+  }
+
+
+  export default handleGetTranscriptRequest;
